613228b4a36b79f394f2c59b956337c25367468c
Aiden Vigue <aiden@vigue.me>
2025-04-19 23:59:27 -0400

    changes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 import { generate_jwt, extract_csr } from './helpers'
 import { jwtVerify } from 'jose'
 import { Agent } from 'https'
+import { type D1Database } from '@cloudflare/workers-types'
 
 const agent = new Agent({
   rejectUnauthorized: false
@@ -12,7 +13,15 @@ type Bindings = {
   PROVISIONING_JWK: string
   STEP_CA_HOST: string
   TOKEN_SECRET: string
+  db: D1Database
 }
+
+const dsParamsSchema = z.object({
+  ds_key_id: z.number().min(1).max(7),
+  rsa_len: z.number().min(31).max(128),
+  cipher_c: z.string().base64(),
+  iv: z.string().base64()
+});
 
 const app = new Hono<{ Bindings: Bindings }>()
 
@@ -101,6 +110,56 @@ app.post('/sign', async (c) => {
 
   return c.body(pem, 200)
 })
+
+app.post('/ds_params', async (c) => {
+  const device_id = c.req.header('x-device-id')
+
+  if (!device_id) {
+    return c.text('Missing device_id!', 400)
+  }
+
+  const json = await c.req.json()
+
+  if (!device_id) {
+    return c.text('Missing device_id!', 400)
+  }
+
+  const dsParams = dsParamsSchema.safeParse(json)
+  if (!dsParams.success) {
+    return c.text('Invalid ds_params!', 400)
+  }
+
+  //check if the device_id exists in the database
+  const data = await c.env.db.prepare('SELECT * FROM ds_data WHERE device_id = ?').bind(device_id).first()
+  if (data) {
+    return c.json({
+      status: 'ok',
+    });
+  }
+
+  //insert the device_id into the database
+  await c.env.db.prepare('INSERT INTO ds_data (device_id, ds_json) VALUES (?, ?)').bind(device_id, JSON.stringify(dsParams.data)).run()
+  return c.json({
+    status: 'ok',
+  });
+});
+
+app.get('/ds_params', async (c) => {
+  const device_id = c.req.header('x-device-id')
+
+  if (!device_id) {
+    return c.text('Missing device_id!', 400)
+  }
+
+  //check if the device_id exists in the database
+  const data = await c.env.db.prepare('SELECT * FROM ds_data WHERE device_id = ?').bind(device_id).first()
+  if (!data) {
+    return c.text('Device not found!', 404)
+  }
+
+  //@ts-expect-error - we're not typing cause we're lazy
+  return c.json(data.ds_json);
+});
 
 app.get('/', async (c) => {
   return c.redirect("https://github.com/koiosdigital/device-provisioning-api")
